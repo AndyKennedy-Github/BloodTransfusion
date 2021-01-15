78cf12cb7b04fdfd5c86539bd60de2cd2de09c4b
Paul Diefenbach <paul@enablegames.com>
2021-01-15 15:51:47 -0500

    Added Speaker1 & 2 commands
    see SpeakerTest.txt:
    Speaker1 'I am speaker 1'
    Wait 1
    Speaker2 'I am speaker 2'
    Wait 1
    Speaker1 'Hello'
    Wait 1
    Speaker2 ''    #clears dialog box

--- BT/Assets/Scripts/MyGameManager.cs
+++ BT/Assets/Scripts/MyGameManager.cs
@@ -179,7 +179,7 @@ public class MyGameManager : MonoBehaviour
 {
     public string [] CommandFiles; //contains list of Scenario files
     private int currentNestingLevel=0;
-    public string Prompt;
+    public string Prompt,Prompt1,Prompt2;
     Dictionary<string, string> fullnames = new Dictionary<string, string>();
     Dictionary<string, int> label = new Dictionary<string, int>();
     Dictionary<string, float> variables = new Dictionary<string, float>();
@@ -201,6 +201,8 @@ public class MyGameManager : MonoBehaviour
         "create",
         "goto",
         "prompt",
+        "speaker1",
+        "speaker2",
         "label"
     };
 
@@ -418,6 +420,14 @@ public class MyGameManager : MonoBehaviour
                     //Prompt = cparams;//(string) ep.EvaluateParam(cparams);
                     Prompt = (string) ep.EvaluateParam(cparams);
                 }
+                if (command == "speaker1"){   //Prompt dialog message
+                    //Prompt = cparams;//(string) ep.EvaluateParam(cparams);
+                    Prompt1 = (string) ep.EvaluateParam(cparams);
+                }
+                if (command == "speaker2"){   //Prompt dialog message
+                    //Prompt = cparams;//(string) ep.EvaluateParam(cparams);
+                    Prompt2 = (string) ep.EvaluateParam(cparams);
+                }
                 if (command == "label"){     //Label a line number in the Scenario file
                     string paramStr = cparams;
                     label.AddSafe(paramStr,i);
@@ -534,10 +544,12 @@ public class MyGameManager : MonoBehaviour
         int paramStart = 2;
         if (GMcommands.Contains(objName.ToLower())){ //GM command without GM
             command = objName;
+            print("GM command without GM:"+command);
             paramStart = 1;
         }else{  //GM or other object
             if (splitArray.Length>1)
                 command = splitArray[1];  
+            print(objName + " command:"+command);
         }
         print("command="+command);
 
@@ -657,6 +669,10 @@ public class MyGameManager : MonoBehaviour
 
             //print("=========================================Object FULLNAME = "+ fullname);
             GameObject go=GameObject.Find(fullname);
+            if (go==null){
+                Debug.Log("ERROR in scenario file: GameObject not found for "+ fullname);
+                return false;
+            }
             if (!fullname.Contains("/")){ //get full name with path and add to dictionary
                 fullname = (string.Join("/", go.GetComponentsInParent<Transform>().Select(t => t.name).Reverse().ToArray()));
                 fullnames[objName]= fullname;
@@ -722,12 +738,24 @@ public class MyGameManager : MonoBehaviour
 
         //GUIStyle style = new GUIStyle(GUI.skin.textArea);
 
+        int border=10;
+        float dialogWidth = 0.8f;
+        style.fontSize = Screen.height/10; //change the font size 
+        if (Prompt1!=null && Prompt1 != ""){
+           GUI.TextArea(new Rect(border, Screen.height*.4f, Screen.width*dialogWidth, Screen.height/5), Prompt1,style);
+        }
+        style.fontSize = Screen.height/10; //change the font size 
+        if (Prompt2!=null && Prompt2 != ""){
+           GUI.TextArea(new Rect(Screen.width* (1-dialogWidth) - border, Screen.height*.6f, Screen.width*dialogWidth-border, Screen.height/5), Prompt2,style);
+//           GUI.Label(new Rect(10, 10, 100, 20), cs.commandLine ); 
+        }
+
+
+
         style.fontSize = Screen.height/10; //change the font size 
         if (Prompt!=null && Prompt != ""){
-
            GUI.TextArea(new Rect(0, Screen.height*.8f, Screen.width, Screen.height/5), Prompt,style);
 //           GUI.Label(new Rect(10, 10, 100, 20), cs.commandLine );
- 
         }
     }
 
