6a2b27aab4e22e693cb484173310a89bfd4b2b63
Tony A. Rowe <Bagelpriest@users.noreply.github.com>
2021-01-16 18:24:13 -0500

    Level Gen mods, added DOTween
    Incorporated DOTween and HOTween.
    
    Modified Level Gen system as discussed with Paul:
    1. Room SubObjects now instantiate as children of their parent Room Objects
    2. Multiple SubObjects may be spawned as children of a single Room Object. The Level Gen Excel tool is currently limited to 4 SubObjects per object, but there is no theoretical limit if the Level Gen text file is modified.
    3. SubObjectParent script may be added to a prefab object. It will automatically find all child Transforms and turn them into valid SubObject locations.
    4. If you have a prefab where you don't want all children to be used as SubObject locations, the SubObjectParentFixed allows the user to choose which child Transforms to use in the Unity editor.
    5. A Room Object prefab without one of the SubObjectParent scripts above will not spawn SubObjects.
    6. If a Room Object should spawn on a non-standard location, place an empty Game Object in the correct location and replace the spawn location name in the text file with the name of the empty Game Object.

---
+++ BT/Assets/Scripts/LevelGen/SubObjectParentFixed.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubObjectParentFixed : SubObjectParent
+{
+    void Awake()
+    {
+        // Asset creator must set all transforms that are used as SubObjectLocs in the Unity editor
+    }
+}
