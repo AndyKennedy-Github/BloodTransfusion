c4fbf3956eb3cee587a2f57c2d6fcd4d9c913952
Andrew Kennedy <aek5594@psu.edu>
2021-01-24 21:17:56 -0500

    Finished Scene06Alpha

---
+++ BT/Assets/Scripts/SalineDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SalineDisplay : MonoBehaviour
+{
+
+    public TextMeshProUGUI saline;
+    public ObjectMessageHandler tracker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        saline.text = tracker.salineAmount.ToString();
+    }
+}
